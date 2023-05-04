ddef4626065e6056b215d94ca9b04a9126f2e4eb
Nuri can Kalkan <58035364+johnashield13@users.noreply.github.com>
2023-05-04 14:24:08 +0300

    Create lesson_01.py

---
+++ PythonAcademia/lesson_01.py
@@ -0,0 +1,12 @@
+# python Yorum satırları # ile oluşturulur
+"""
+       Birden fazla satırda yorum eklenmek isteniyorsaüç tırnak işaretleri arasına ifadesi arasına eklenebilir
+"""
+print("Hello Python !!!")
+# Yukarıda yazan ifade bize Hello Python !!! ifadesini konsol üzerinde yazdırmamızı sağlayacaktır
+# Bu ifadeyi çalıştırabilmek için "Terminal" üzerinde "New Terminal" ifadesini açıp "py dosyaAdi.py" yazmamız gereklidir
+
+input("Bir veri giriniz : ")
+# input() ifadesi konsol üzerinde kullanıcının bir veri girebileceği ortam oluşturur.
+# girilen bu veri "String" bir değer olarak tanımlanır.
+
